32071ecfe0c485b276d8465a2c70496a24d39c71
Francisco Power <franciscopower66@gmail.com>
2023-10-27 20:54:43 +0100

    testing threads

--- GPTerminal/src/Source.cpp
+++ GPTerminal/src/Source.cpp
@@ -7,8 +7,45 @@
 #include "PowerTUI.h"
 
 
+class ThreadsClass
+{
+public:
+	ThreadsClass() {};
+	~ThreadsClass() {};
+
+	void setVar(bool classBool) {
+		this->runLoop = classBool;
+	}
+
+	void classLoop() {
+		int n = 0;
+		while (runLoop) {
+			n++;
+			std::cout << n << " ";
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+	}
+
+private:
+
+	bool runLoop = true;
+
+};
+
+
+
 int main(){
 
+	ThreadsClass myThreadsClass = ThreadsClass();
+
+	std::thread firstThread(&ThreadsClass::classLoop, &myThreadsClass);
+
+
+	std::this_thread::sleep_for(std::chrono::seconds(5));
+
+	myThreadsClass.setVar(false);
+
+	firstThread.join();
 
     return 0;
 }
