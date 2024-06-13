0025f0701ab1bc57198471576975fe208f5a3719
Francisco Power <33499683+franciscopower@users.noreply.github.com>
2024-06-13 16:19:48 +0100

    update openai model list

--- GPTerminal/include/OpenAiApi.h
+++ GPTerminal/include/OpenAiApi.h
@@ -13,23 +13,11 @@ public:
 	Result<std::string, std::string> createPrompt(std::string user_input);
 	Result<std::string, std::string> decodeReply(std::string api_reply);
 
-	const char* MODELS_LIST[16] = {
-      "gpt-4-0613",
+	const char* MODELS_LIST[4] = {
+      "gpt-4o",
       "gpt-4",
-      "gpt-4-1106-preview",
       "gpt-4-turbo",
-      "gpt-4-turbo-2024-04-09",
-      "gpt-3.5-turbo-16k",
-      "gpt-3.5-turbo-0613",
-      "gpt-3.5-turbo-0301",
-      "gpt-3.5-turbo-instruct-0914",
-      "gpt-3.5-turbo-0125",
-      "gpt-3.5-turbo-instruct",
-      "gpt-4-0125-preview",
       "gpt-3.5-turbo",
-      "gpt-3.5-turbo-16k-0613",
-      "gpt-4-turbo-preview",
-      "gpt-3.5-turbo-1106",
 	};
 
 };
